5d55608cad9b4b3727f1bf5540173ae9fd7da2f0
DavidPinilla <davidpinillame1995@gmail.com>
2023-01-23 15:18:17 -0500

    proyecto actualizado

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,17 +1,32 @@
-import { React } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import  React, { useState }  from "react";
+import { View, Text, TextInput, Button, TouchableWithoutFeedback, Keyboard} from "react-native";
 import { styles } from "./styles";
 import { Card } from "../../components";
 import { colors } from "../../constants";
 
 
 export const StartGame = () => {
+   const [enteredValue, setEnteredValue] = useState("");
+
+   const onHandlerChange = (text) => {
+    setEnteredValue(text.replace(/[^0-9]/g, ""));
+   };
+
 return (
+<TouchableWithoutFeedback onPress={() => {
+    Keyboard.dismiss();
+}}>  
     <View style={styles.container}>
         <Text style={styles.titlee}>Comenzar juegoo</Text>
         <Card style={styles.inputContainer}>
             <Text style={styles.label}>Escribe un numero</Text>
-            <TextInput keyboardType="numeric" style={styles.input} placeholder="0"/>
+            <TextInput 
+            value={enteredValue}
+            keyboardType="numeric" 
+            style={styles.input} 
+            placeholder="0"
+            onChangeText={onHandlerChange}
+            />
             <View style={styles.buttonContainer}>
                  <Button 
                   title="Reiniciar"
@@ -26,6 +41,7 @@ return (
             </View>   
         </Card>
     </View>
+    </TouchableWithoutFeedback>  
     )
 }
 
