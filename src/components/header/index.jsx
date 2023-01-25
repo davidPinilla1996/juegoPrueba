f9bc4e90822a64050576a92d88e91bf5e5c326fb
DavidPinilla <davidpinillame1995@gmail.com>
2023-01-24 19:40:06 -0500

    proyecto nuevo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,17 @@
 //importamos react
 import React from "react";
+//importando componentes
 import { Text, View } from "react-native";
+//importamnos el styles para estilizar mi app
 import { styles } from "./styles";
 
-//declaramos nuestro componente que en este caso el el header
-//recibe como propiedad el titulo que se va a mostrar en la cabezera (title) va  ser un componente reutilizable
+//declaramos nuestro componente que en este caso es el header
+//recibo como propiedad el titulo que se va a mostrar en la cabezera (title)
 const Header = ({title}) => {
     return (
+        //retornamos el componente View con el componente Text 
        <View style={styles.container}>
+             {/* El componente Text va a recibir el title (titulo) por que va a ser dinamico*/}
             <Text style={styles.title}>{title}</Text>
        </View>
     )
