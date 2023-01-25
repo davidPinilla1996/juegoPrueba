0e867fa0dd83c4adc38023f5f727eefac4c8a689
DavidPinilla <davidpinillame1995@gmail.com>
2023-01-25 12:22:50 -0500

    proyecto nuevo

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,16 +1,35 @@
 import  React, { useState }  from "react";
-import { View, Text, TextInput, Button, TouchableWithoutFeedback, Keyboard} from "react-native";
+import { View, Text, TextInput, Button, TouchableWithoutFeedback, Keyboard, Alert} from "react-native";
 import { styles } from "./styles";
 import { Card } from "../../components";
 import { colors } from "../../constants";
 
 
 export const StartGame = () => {
-   const [enteredValue, setEnteredValue] = useState("");
 
+    const [enteredValue, setEnteredValue] = useState("");
+    const [confirmed, setConfirmed] = useState(false);
+//con esta funcion valido que todo lo que se escriba en el campo sea unicamente un numero del 1 hasta el 99
    const onHandlerChange = (text) => {
     setEnteredValue(text.replace(/[^0-9]/g, ""));
    };
+
+   //con esta funcion reseteo el campo de escritura del numero.
+const onHandleReset = () => {
+    //Estamos setiando el valor por defecto del TextInput para al darle click al boton reinical se reinicie el campo
+    setEnteredValue("");
+    //reinica el estado de confirmacion del TextInput
+    setConfirmed(false);
+};
+//evalua el numero que yo escriba dentro del campo
+const onHandleConfirm = () => {
+    //convertimos el valor o el numero ingresado en el campo a un numero, lo parciamos
+   const chosenNumber = parseInt(enteredValue, 10);
+   if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+       Alert.alert("Numero invalido", "el numero tiene que estar entre el 1 y el 99", [{text: "Entendido", style: "destructive", onPress: onHandleReset}]);
+   }
+};
+
    
 return (
 <TouchableWithoutFeedback onPress={() => {
@@ -22,7 +41,7 @@ return (
             <Text style={styles.label}>Escribe un numero</Text>
             <TextInput 
             value={enteredValue}
-            keyboardType="numeric" 
+            keyboardType="number-pad" 
             style={styles.input} 
             placeholder="0"
             onChangeText={onHandlerChange}
@@ -30,12 +49,12 @@ return (
             <View style={styles.buttonContainer}>
                  <Button 
                   title="Reiniciar"
-                  onpress={() => null}
+                  onpress={onHandleReset}
                   color={colors.secondary}
                  />
                  <Button 
                   title="Confirmar"
-                  onpress={() => null}
+                  onPress={onHandleConfirm}
                   color={colors.primary}
                  />
             </View>   
