42fd891b3300f51262e3235038a6c7d2a9e9b184
DavidPinilla <davidpinillame1995@gmail.com>
2023-01-28 22:20:19 -0500

    proyecto actualizado

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-//exportamos los componentes para que puedan funcionar en mi app
+//hacer esto se le suele llamar barrin o barrel en ingles 
+//signifa que tengo un contendedor que contiene el modulo de  mis componentes 
 export {default as Header} from './header/index';
 export { default as Card } from './card/index';
 export { default as NumberContainer } from './number-container/index';
