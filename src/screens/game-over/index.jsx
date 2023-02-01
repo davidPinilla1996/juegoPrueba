af29630de60f3ed6af08a32639f98d051637a8e5
DavidPinilla <davidpinillame1995@gmail.com>
2023-02-01 11:10:10 -0500

    proyecto actualizado

--- src/screens/game-over/index.jsx
+++ src/screens/game-over/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Button, Text, View } from 'react-native';
+import { Button, ImageBackground, Text, View } from 'react-native';
 import { styles } from './styles';
 import { Card } from '../../components';
 import { colors } from '../../constants';
 
 const GameOver = ({rounds, selectedNumber, onHandleRestartGame}) => {
   return (
+<ImageBackground source={(require("../../img/juegoDelCalamarGameOver.jpg"))} style={styles.image}>
     <View style={styles.container}>
         <Card style={styles.content}>
             <Text style={styles.rondasText}>Rondas: {rounds}</Text>
@@ -13,10 +14,11 @@ const GameOver = ({rounds, selectedNumber, onHandleRestartGame}) => {
             <Button 
                title="Reiniciar juego" 
                onPress={onHandleRestartGame}
-               color={colors.primary}
+               color={colors.red}
             />
         </Card>
-    </View>
+    </View>   
+</ImageBackground>  
   );
 };
 
--- src/screens/game-over/styles.js
+++ src/screens/game-over/styles.js
@@ -1,9 +1,9 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../../constants";
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center"
     },
@@ -11,12 +11,35 @@ export const styles = StyleSheet.create({
         width: "80%",
         paddingVertical: 20,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        backgroundColor: colors.black,
+        borderBottomEndRadius: 40,
+        borderTopLeftRadius: 40,
+        shadowColor: "#d60707",
+        backgroundColor: colors.backgroundFonds,
+        shadowOffset: {
+	    width: 0,
+	    height: 12,
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.00,
+        elevation: 24,
     },
     rondasText: {
       fontSize: 16,
       textAlign: "center",
       paddingVertical: 10,
       fontFamily: "Orbitron-Regular",
-    }
+      color: colors.white
+    },
+    selectedNumber: {
+       color: colors.text,
+       marginBottom: 10,
+       fontSize: 18,
+       fontFamily: "Orbitron-Regular",
+    },
+    image: {
+        width: "100%",
+        height: "93%"
+      },
 });
